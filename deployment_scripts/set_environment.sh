011e641c68d5acea7e196474720ce1011f2f7aed
renjithstalin <53550078+renjithstalin@users.noreply.github.com>
2020-10-21 15:57:41 -0500

    Update set_environment.sh

--- deployment_scripts/set_environment.sh
+++ deployment_scripts/set_environment.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 export DEFAULT_LOCATION=uksouth
-export SIRMIONE_ALIAS=bpsirmione
-export LIMONE_ALIAS=bplimone
-export SCORPIO_ALIAS=bpscorpio
-export VIRGO_ALIAS=bpvirgo
-export LIBRA_ALIAS=bplibra
-export ZODIAC_ALIAS=bpzodiac
-export DB_ADMIN_USER=nick
+export SIRMIONE_ALIAS=rs1147zodiac
+export LIMONE_ALIAS=rs1147zodiac
+export SCORPIO_ALIAS=rs1147zodiac
+export VIRGO_ALIAS=rs1147zodiac
+export LIBRA_ALIAS=rs1147zodiac
+export ZODIAC_ALIAS=rs1147zodiac
+export DB_ADMIN_USER=rs1147zodiac
 export AAD_DOMAIN=xekina.onmicrosoft.com
 export AAD_TENANTID=3bc03625-3a0a-48c5-8aa5-12f22e401fff
 export AAD_CLIENTID=ccee7608-940c-42d9-ba86-a2845ef3a808
@@ -19,12 +19,12 @@ export ZODIAC_INSTANCE=Jemima
 echo "<h2>Environment Variables</h2>" >> deployment-log.html
 echo "<p>ZODIAC_INSTANCE:$ZODIAC_INSTANCE</p>" >> deployment-log.html
 echo "<p>DEFAULT_LOCATION:$DEFAULT_LOCATION</p>" >> deployment-log.html
-echo "SIRMIONE_ALIAS:$rs1147zodiac" >> deployment-log.html
-echo "LIMONE_ALIAS:$rs1147zodiac" >> deployment-log.html
-echo "SCORPIO_ALIAS:$rs1147zodiac" >> deployment-log.html
-echo "VIRGO_ALIAS:$rs1147zodiac" >> deployment-log.html
-echo "LIBRA_ALIAS:$rs1147zodiac" >> deployment-log.html
-echo "ZODIAC_ALIAS:$rs1147zodiac" >> deployment-log.html
+echo "SIRMIONE_ALIAS:$SIRMIONE_ALIAS" >> deployment-log.html
+echo "LIMONE_ALIAS:$LIMONE_ALIASc" >> deployment-log.html
+echo "SCORPIO_ALIAS:$SCORPIO_ALIAS" >> deployment-log.html
+echo "VIRGO_ALIAS:$VIRGO_ALIAS" >> deployment-log.html
+echo "LIBRA_ALIAS:$LIBRA_ALIAS" >> deployment-log.html
+echo "ZODIAC_ALIAS:$ZODIAC_ALIAS" >> deployment-log.html
 echo "DB_ADMIN_USER:$DB_ADMIN_USER" >> deployment-log.html
 echo "AAD_DOMAIN:$AAD_DOMAIN" >> deployment-log.html
 echo "OUTPUT:$OUTPUT" >> deployment-log.html
